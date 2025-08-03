b813955d7d879edbdb93abea990b45b4197685ae
kamrankhan67 <mr.kamrankhan67@gmail.com>
2025-08-03 20:43:45 +0500

    2nd commit

--- lib/components/chat_bubble.dart
+++ lib/components/chat_bubble.dart
@@ -1,18 +1,24 @@
 import 'package:flutter/material.dart';
 
 class ChatBubble extends StatelessWidget {
-  const ChatBubble({super.key,required this.isCurrentUser,required this.message});
+  const ChatBubble({
+    super.key,
+    required this.isCurrentUser,
+    required this.message,
+  });
   final bool isCurrentUser;
   final String message;
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: EdgeInsets.all(10),
+      margin: EdgeInsetsGeometry.all(10),
       decoration: BoxDecoration(
-        color: isCurrentUser?Colors.blue:Colors.green,
-        borderRadius: BorderRadius.circular(20)
+        color: isCurrentUser ? Colors.blue : Colors.green,
+        borderRadius: BorderRadius.circular(20),
       ),
-      child:Text(message),
+      child: Text(message),
     );
   }
 }
